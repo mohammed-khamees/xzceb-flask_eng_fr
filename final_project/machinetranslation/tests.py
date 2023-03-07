a01f52fa06bc01b783c5e6995bdbe29720d5b929
Mohammad Khamees <Mohammad.Khamees@ip-10-72-104-83.eu-west-1.compute.internal>
2023-03-07 09:34:00 +0400

    added the translation functions

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+from translator import english_to_french, french_to_english
+
+
+class TestEnglishToFrench(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertNotEqual(english_to_french('How are you?'), 'Bonjour')
+
+
+class TestFrenchToEnglish(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        self.assertNotEqual(french_to_english('Bonjour'), 'How are you?')
+
+
+unittest.main()
---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,32 @@
+"""Module providingFunction evnviroments variable."""
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+
+def english_to_french(english_text):
+    """providingFunction translates english_to_french"""
+    french_text = language_translator.translate(
+        english_text, model_id='en-fr').get_result()
+    return french_text.get('translations')[0].get('translation')
+
+
+def french_to_english(french_text):
+    """providingFunction translates french_to_english"""
+    english_text = language_translator.translate(
+        french_text, model_id='fr-en').get_result()
+    return english_text.get('translations')[0].get('translation')
